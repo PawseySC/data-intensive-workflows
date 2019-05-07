b6b5c7bd26a486ab868a2ad07720fa567c58225e
Marco De La Pierre <marco.delapierre@gmail.com>
2019-05-07 15:10:50 +0800

    nextflow minor mods

--- nextflow/3.containers-blast/blast-shifter-run-queue.sh
+++ nextflow/3.containers-blast/blast-shifter-run-queue.sh
@@ -11,6 +11,6 @@
 module load shifter
 
 # run BLAST with containers
-srun --export=all shifter run biocontainers/blast:v2.2.31_cv2 makeblastdb -in zebrafish.1.protein.faa -dbtype prot
+srun --export=all shifter run biocontainers/blast:v2.2.31_cv2  makeblastdb -in zebrafish.1.protein.faa -dbtype prot
 
-srun --export=all shifter run biocontainers/blast:v2.2.31_cv2 blastp -query P04156.fasta -db zebrafish.1.protein.faa -out results.txt
+srun --export=all shifter run biocontainers/blast:v2.2.31_cv2  blastp -query P04156.fasta -db zebrafish.1.protein.faa -out results.txt
