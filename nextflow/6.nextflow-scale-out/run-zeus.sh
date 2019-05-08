454b8b8da36cf63d836df65224be89d7f6ee9992
Marco De La Pierre <marco.delapierre@gmail.com>
2019-05-08 16:08:16 +0800

    nextflow mods to eps 05 06

--- nextflow/6.nextflow-scale-out/run-zeus.sh
+++ nextflow/6.nextflow-scale-out/run-zeus.sh
@@ -1,10 +1,3 @@
-#!/bin/bash -l
+#!/bin/bash
   
-#SBATCH --job-name=Nextflow-master-RNAseq
-#SBATCH --account=pawsey0001
-#SBATCH --reservation=DSW
-#SBATCH --partition=workq
-#SBATCH --ntasks=1
-#SBATCH --time=02:00:00
- 
-nextflow run rnaseq.nf
+nextflow run rnaseq.nf -profile zeus
